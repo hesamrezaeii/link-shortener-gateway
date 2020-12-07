25a8252fa5037a3d00402163f81ec44c83d71659
hesam <hesam@hesam.com>
2020-12-07 14:39:02 +0330

    second-edit

--- src/main/java/com/example/firstproject/control/Controller.java
+++ src/main/java/com/example/firstproject/control/Controller.java
@@ -1,34 +1,27 @@
 package com.example.firstproject.control;
 
-import com.example.firstproject.db.ClickHourlyRepository;
-import com.example.firstproject.db.UrlClickRepository;
 import com.example.firstproject.db.UrlRepository;
 import com.example.firstproject.service.ClickHourly;
 import com.example.firstproject.service.Producer;
 import com.example.firstproject.service.Url;
 import com.example.firstproject.service.UrlClick;
-import com.sun.istack.internal.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+
 @RestController
 public class Controller {
 
-    private  Producer producer;
+    private Producer producer;
     @Autowired
     private UrlRepository urlRepository;
     @Autowired
-    private UrlClickRepository urlClickRepository;
-    @Autowired
-    private ClickHourlyRepository clickHourlyRepository;
-    @Autowired
-    private KafkaTemplate kafkaTemplate ;
+    private KafkaTemplate kafkaTemplate;
 
     @GetMapping("/all")
     List<Url> all() {
@@ -38,25 +31,18 @@ public class Controller {
 
         return urls;
     }
-    @GetMapping("/clicksStat")
-    List<ClickHourly> ClickHourly() {
 
-        List<ClickHourly> stats = clickHourlyRepository.findAll().stream()
-                .collect(Collectors.toList());
+    @GetMapping("/ten-short-urls")
+    List<String> shortUrl() {
 
-        return stats;
+        List<String> shortUrls = urlRepository.findAll()
+                .stream().map(Url::getShortUrl).
+                        limit(10).collect(Collectors.toList());
+
+
+        return shortUrls;
     }
-    @GetMapping("/pastHourClick")
-    List<UrlClick> allUrlClickFromHourAg() {
 
-        Date now = new Date();
-
-        Date hourAgo = new Date(System.currentTimeMillis() - (60 * 60 * 1000));
-
-        List<UrlClick> urls = urlClickRepository.findByCreatedDateBetween(hourAgo,now);
-
-        return urls;
-    }
     @GetMapping("/{shortUrl}")
     Url originallyUrl(@PathVariable String shortUrl) {
         Url url = urlRepository.findByShortUrl(shortUrl); //
@@ -64,7 +50,7 @@ public class Controller {
     }
 
     @PostMapping("/")
-    Url shortUrl(HttpServletRequest request , @RequestBody Url url) {
+    Url shortUrl(HttpServletRequest request, @RequestBody Url url) {
         Url exist = urlRepository.findByOriginalUrl(url.getOriginalUrl());
         if (exist != null) {
             return null;
@@ -86,18 +72,15 @@ public class Controller {
 
     @PostMapping("/click/{url}")
     void click(@PathVariable String url) {
-//        Url url1 = new Url(url.getOriginalUrl(),url.getShortUrl());
-//        System.out.println(url1);
-       kafkaTemplate.send("urlstats",url);
+        Url exist = urlRepository.findByShortUrl(url);
+        if (exist != null) {
+            kafkaTemplate.send("urlstats", url);
+        }
     }
 
     @DeleteMapping("/all")
     String deleteAll() {
-
         urlRepository.deleteAll();
-        urlClickRepository.deleteAll();
-        clickHourlyRepository.deleteAll();
-
-        return " Cleared Collections";
+        return "cleared";
     }
 }
